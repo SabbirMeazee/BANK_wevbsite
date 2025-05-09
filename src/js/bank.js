4bc8a5ff4bd76f23731a23497f7ccde73d456b96
SabbirMeazee <mdsabbirmeazee69@gmail.com>
2025-05-10 02:14:35 +0600

    validation added

--- src/js/bank.js
+++ src/js/bank.js
@@ -1,30 +1,46 @@
-// handle deposit button event
+//=====Deposit=================
 document.getElementById('deposit-button').addEventListener('click', function () {
-    // get the amount deposited
-    const depositInput = document.getElementById('deposit-input').value;
-    const DepositAmount = parseFloat(depositInput);
-    // update deposit total
-    const depositTotal = document.getElementById('deposit-total').innerText;
-    const previousDepositAmount = parseFloat(depositTotal);
-    const newDepositTotal = previousDepositAmount + DepositAmount;
+    const depositInput = document.getElementById('deposit-input');
+    const DepositValue = depositInput.value;
+    const newDepositAmount = parseFloat(DepositValue);
+
+    if (DepositValue === "" || isNaN(parseFloat(DepositValue)) || (parseFloat(DepositValue) < 0)) {
+        document.getElementById('invalid').innerText = 'Please Enter Valid Amount !!!!'
+        depositInput.value = ''
+        return;
+    }
+    document.getElementById('invalid').innerText = ''
+
+
+    const depositTotal = document.getElementById('deposit-total');
+    const previousDeposit = depositTotal.innerText;
+    const previousDepositAmount = parseFloat(previousDeposit);
+    const newDepositTotal = previousDepositAmount + newDepositAmount;
 
     depositTotal.innerText = newDepositTotal;
 
     // update account balance 
-    const balanceTotal = document.getElementById('balance-total').innerText;
-    const previousBalanceTotal = parseFloat(balanceTotal);
-    const newBalanceTotal = previousBalanceTotal + DepositAmount;
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+    const newBalanceTotal = previousBalanceTotal + newDepositAmount;
     balanceTotal.innerText = newBalanceTotal;
     // clear the deposit input field
     depositInput.value = '';
 });
 
-// handle withdraw event handler
+////////// ================Withdraw==========///////////////
 document.getElementById('withdraw-button').addEventListener('click', function () {
-    const withdrawInput = document.getElementById('withdraw-input').value;
-    const newWithdrawAmount = parseFloat(withdrawInput);
-    console.log(newWithdrawAmount);
+    const withdrawInput = document.getElementById('withdraw-input');
+    const withdrawValue = withdrawInput.value;
+    const newWithdrawAmount = parseFloat(withdrawValue);
 
+    if (withdrawValue === "" || isNaN(parseFloat(withdrawValue)) || (parseFloat(withdrawValue) < 0)) {
+        document.getElementById('invalid').innerText = 'Please Enter Valid Amount !!!!'
+        withdrawValue.value = ''
+        return;
+    }
+    document.getElementById('invalid').innerText = ''
     // set withdraw total
     const withdrawTotal = document.getElementById('withdraw-total');
     const previousWithdrawText = withdrawTotal.innerText;
