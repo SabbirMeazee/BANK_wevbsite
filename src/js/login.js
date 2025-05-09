5ecb7c9c53dec732382f02d45f5c2d3f1f4eaa42
SabbirMeazee <mdsabbirmeazee69@gmail.com>
2025-05-10 03:19:28 +0600

    error handaled

--- src/js/login.js
+++ src/js/login.js
@@ -7,7 +7,7 @@ document.getElementById('login-submit').addEventListener('click', function () {
     const userPassword = passwordField.value;
     // check email and password
     if (userEmail == 'sabbir@gmail.com' && userPassword == '123456') {
-        window.location.href = 'bank.html';
+        window.location.href = './src/bank.html';
     }
     else {
         document.getElementById('error').innerText = "Invalid Input! Please Enter valid input."
